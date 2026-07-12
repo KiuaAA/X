81b99264168b52f50ccb8b4fd3fef868c6e1b6b2
KiuaX <kiuax26@gmail.com>
2026-07-13 09:56:08 +1400

    Update MainActivity.kt

--- XL/src/main/java/com/x/launcher/MainActivity.kt
+++ XL/src/main/java/com/x/launcher/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.x.launcher
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.x.client.XClientService
 import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.x.client.XClientService
+import com.x.launcher.ui.screens.HomeScreen
+import com.x.launcher.ui.theme.XTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Starts the X client background service that keeps
-        // downloads, mod indexing, and JVM prep running smoothly.
         startService(Intent(this, XClientService::class.java))
+
+        setContent {
+            XTheme {
+                HomeScreen(
+                    onPlay = { /* wired to XClientService.launchGame in the next part */ }
+                )
+            }
+        }
     }
 }
