776753c254ba90e98f5a388586959f498e02127b
KiuaX <kiuax26@gmail.com>
2026-07-13 09:51:42 +1400

    Create Color.kt

---
+++ XL/src/main/java/com/x/launcher/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.x.launcher.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val XBlack = Color(0xFF0A0A0A)
+val XCream = Color(0xFFF3EFA6)
+val XCreamDim = Color(0xFFE9E38C)
+val XCardBlack = Color(0xFF141414)
+val XTextOnCream = Color(0xFF141414)
+val XTextOnBlack = Color(0xFFF5F5F5)
