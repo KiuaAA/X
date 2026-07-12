f57712339925a287562a89aa6dd09fa6bc018255
KiuaX <kiuax26@gmail.com>
2026-07-13 09:50:38 +1400

    Update build.gradle.kts

--- XL/build.gradle.kts
+++ XL/build.gradle.kts
@@ -21,6 +21,14 @@ android {
         }
     }
 
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -28,7 +36,16 @@ android {
 
 dependencies {
     implementation(project(":xclient"))
+
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
 }
