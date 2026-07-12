ec2d6000fee8c9d7143837d4d90400c949f3c678
KiuaX <kiuax26@gmail.com>
2026-07-13 09:10:09 +1400

    Create CrashSolutions.kt

---
+++ xclient/src/main/java/com/x/client/download/CrashSolutions.kt
@@ -0,0 +1,8 @@
+package com.x.client.download
+
+object CrashSolutions {
+    fun suggest(message: String): String {
+        // Real mapping table comes in the crash-parser part.
+        return "Check your internet connection and storage permissions, then retry."
+    }
+}
