8612a63e1f51da93f8a4e311b81db1f27f13172a
KiuaX <kiuax26@gmail.com>
2026-07-13 22:55:53 +1400

    Update build.gradle.kts

--- XL/build.gradle.kts
+++ XL/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
     implementation("io.coil-kt:coil-compose:2.6.0")
 
