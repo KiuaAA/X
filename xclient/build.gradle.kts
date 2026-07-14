2cb061042744e037fbdb2d4f111fc84903d8aea0
KiuaX <kiuax26@gmail.com>
2026-07-14 23:08:11 +1400

    Update build.gradle.kts

--- xclient/build.gradle.kts
+++ xclient/build.gradle.kts
@@ -12,6 +12,11 @@ android {
         targetSdk = 34
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
